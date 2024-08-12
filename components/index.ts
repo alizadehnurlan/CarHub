c67c5a4a94019059ee5db87d4205ee73a70df4d5
alizadehnurlan <alizadehnurlan8@gmail.com>
2024-08-12 13:59:50 +0400

    Navbar and footer

---
+++ components/index.ts
@@ -0,0 +1,11 @@
+import Hero from '../components/Hero/Hero'
+import CustomButton from '../components/CustomButton/CustomButton'
+import Navbar from './Navbar/Navbar'
+import Footer from './Footer/Footer'
+
+export{
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer
+}
